4d784887ec754094fbc35eaf35647abe2c3d941e
Oliver Heilig <oliver.heilig@yahoo.de>
2024-11-10 14:37:32 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WebView2Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webViewControl = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.latitudeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.longitudeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.wayPointBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wayPointBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webViewControl);
+            this.splitContainer1.Size = new System.Drawing.Size(1054, 450);
+            this.splitContainer1.SplitterDistance = 310;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.latitudeDataGridViewTextBoxColumn,
+            this.longitudeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.wayPointBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 350);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Route";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // webViewControl
+            // 
+            this.webViewControl.AllowExternalDrop = true;
+            this.webViewControl.CreationProperties = null;
+            this.webViewControl.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webViewControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webViewControl.Location = new System.Drawing.Point(0, 0);
+            this.webViewControl.Name = "webViewControl";
+            this.webViewControl.Size = new System.Drawing.Size(740, 450);
+            this.webViewControl.TabIndex = 0;
+            this.webViewControl.ZoomFactor = 1D;
+            this.webViewControl.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webViewControl_NavigationCompleted);
+            // 
+            // latitudeDataGridViewTextBoxColumn
+            // 
+            this.latitudeDataGridViewTextBoxColumn.DataPropertyName = "Latitude";
+            this.latitudeDataGridViewTextBoxColumn.HeaderText = "Latitude";
+            this.latitudeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.latitudeDataGridViewTextBoxColumn.Name = "latitudeDataGridViewTextBoxColumn";
+            this.latitudeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // longitudeDataGridViewTextBoxColumn
+            // 
+            this.longitudeDataGridViewTextBoxColumn.DataPropertyName = "Longitude";
+            this.longitudeDataGridViewTextBoxColumn.HeaderText = "Longitude";
+            this.longitudeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.longitudeDataGridViewTextBoxColumn.Name = "longitudeDataGridViewTextBoxColumn";
+            this.longitudeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // wayPointBindingSource
+            // 
+            this.wayPointBindingSource.DataSource = typeof(WebView2Map.WayPoint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wayPointBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewControl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn latitudeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn longitudeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource wayPointBindingSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace WebView2Map
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        private static readonly string apiKey = ""; // Get your free key at https://developer.myptv.com/;
+        public Form1()
+        {
+            InitializeComponent();
+
+            webViewControl.Source = new Uri($"file:///{Application.StartupPath}/routing.html");
+        }
+
+        WayPoint[] wayPoints = new [] {
+            new WayPoint(8.37922, 49.01502),
+            new WayPoint(8.42806, 49.01328),
+        };
+
+        public void onCalculated(int distance, int travelTime)
+        {
+            label1.Text = $"Travel Time: {TimeSpan.FromSeconds(travelTime)}" ;
+            label2.Text = $"Distance: {distance / 1000.0} km";
+        }
+
+        private async void webViewControl_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            // initialize the host bridge
+            await webViewControl.EnsureCoreWebView2Async(null);
+            webViewControl.CoreWebView2.AddHostObjectToScript("bridge", this);
+
+            // initilaize maplibre
+            double lat = 8.4;
+            double lng = 49.015;
+            int zoom = 12;
+            await webViewControl.CoreWebView2.ExecuteScriptAsync(FormattableString.Invariant(
+                $"window.initMapAndRouting('{apiKey}',{lat},{lng},{zoom});"));
+
+            // Calculate initial route
+            calculateRoute(wayPoints);
+        }
+
+        public void waypointsChanged(dynamic waypoints)
+        {
+            // Browser->Host seems to work with [][]double, alternative would be json
+            var wpd = new List<WayPoint>();
+            foreach (dynamic waypoint in waypoints)
+                wpd.Add(new WayPoint(waypoint[0], waypoint[1]));
+
+            var bindingList = new BindingList<WayPoint>(wpd) { AllowNew = true, AllowRemove = true };
+            var source = new BindingSource(bindingList, null);
+            dataGridView1.DataSource = source;
+        }
+
+        private async void calculateRoute(IEnumerable<WayPoint> waypoints) 
+        { 
+            // For complex data Host->Browser just use json
+            var coords = waypoints.Select(wp => new[] {wp.Latitude, wp.Longitude});
+            string json = JsonSerializer.Serialize(coords);
+            await webViewControl.CoreWebView2.ExecuteScriptAsync($"setWaypoints('{json}');");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculateRoute((dataGridView1.DataSource as BindingSource).DataSource as IEnumerable<WayPoint>);
+        }
+    }
+
+    public class WayPoint
+    {
+        public WayPoint()
+        {
+        }
+
+        public WayPoint(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
